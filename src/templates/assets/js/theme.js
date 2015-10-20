366ea82a43ea0fc1edc85a524653c3d83ef931d9
Alex IE Chavet <alex.chavet@ie.com>
2015-10-20 18:50:53 +1100

    V2

--- src/templates/assets/js/theme.js
+++ src/templates/assets/js/theme.js
@@ -21,7 +21,22 @@ function DoSquare(){
 
 
     });
-};
-
+}
 $(document).ready(DoSquare());
 $(window).on("resize", DoSquare());
+
+$(function() {
+    $('a[href*=#]:not([href=\'#myCarousel\'])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname && this.pathname !== '#myCarousel') {
+            
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top -50
+                }, 400);
+                return false;
+            }
+        }
+    });
+});
